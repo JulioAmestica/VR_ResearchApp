eaa609b38ab0e22a27b2cb6dc333e4983ebdd2c8
Julio Amestica <julio.amestica@gmail.com>
2024-05-29 14:57:11 -0400

    Steam

--- Assets/Scripts/ChemestryLabPT.cs
+++ Assets/Scripts/ChemestryLabPT.cs
@@ -11,6 +11,7 @@ public class NewBehaviourScript : MonoBehaviour
     public GameObject Beaker;
     public GameObject BrownBottle;
     public TextMeshProUGUI robotInstructions;
+    public TextMeshProUGUI PTInstructions;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +29,20 @@ public class NewBehaviourScript : MonoBehaviour
         Beaker.SetActive(true);
         BrownBottle.SetActive(true);
         robotInstructions.text="Grab the brown bottle with bromie with the controller  and pour the bromine into the beaker";
+
+        StartCoroutine(StartTimer());
     }
+
+    IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(10.0f);
+        Beaker.SetActive(false);
+        BrownBottle.SetActive(false);
+        PTInstructions.text="Proctice completed, you will now complete a calibration process before conducting this study.";
+        robotInstructions.text="Well done for completing  this practice trial";
+        PTButton.SetActive(true);
+    }
+
 
     // Update is called once per frame
     void Update()
